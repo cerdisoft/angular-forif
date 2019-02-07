4a0ece606172478de25e404d677c0db1424653d7
Manuel Barrios <soporte@adaflex.com.mx>
2019-02-07 17:04:27 -0600

    angular first

---
+++ src/app/components/body/body.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-body',
+    templateUrl: './body.component.html'
+})
+
+export class BodyComponent {
+    frase: any = {
+        mensaje: 'Un gran poder requiere una gran responsabilidad',
+        author: 'Ben Parker'
+    }
+}
